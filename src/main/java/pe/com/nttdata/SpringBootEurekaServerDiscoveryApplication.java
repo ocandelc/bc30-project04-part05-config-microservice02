f54806f0abfbdc37c52d8598abab7bb4ae21147f
ocandelc <oscarerick.candelacarbajal@nttdata.com>
2022-08-26 13:12:01 -0500

    commit

--- src/main/java/pe/com/nttdata/SpringBootEurekaServerDiscoveryApplication.java
+++ src/main/java/pe/com/nttdata/SpringBootEurekaServerDiscoveryApplication.java
@@ -1,9 +1,26 @@
+/**
+ * Resumen.
+ * Objeto                   : SpringBootEurekaServerDiscoveryApplication.java
+ * Descripción              : Clase para iniciar el discovery.
+ * Fecha de Creación        : 08/08/2022.
+ * Proyecto de Creación     : Bootcamp-30.
+ * Autor                    : Oscar Candela.
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * Modificaciones
+ * Motivo                   Fecha             Nombre                  Descripción
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * Bootcamp-30              17/08/2022        Mario Vásquez           Realizar la creación de un método nuevo.
+ */
+
 package pe.com.nttdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Clase para iniciar el discovery.
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class SpringBootEurekaServerDiscoveryApplication {
--- src/test/java/pe/com/nttdata/SpringBootEurekaServerDiscoveryApplicationTest.java
+++ src/test/java/pe/com/nttdata/SpringBootEurekaServerDiscoveryApplicationTest.java
@@ -1,8 +1,25 @@
+/**
+ * Resumen.
+ * Objeto                   : SpringBootEurekaServerDiscoveryApplicationTest.java
+ * Descripción              : Clase para iniciar el discovery.
+ * Fecha de Creación        : 08/08/2022.
+ * Proyecto de Creación     : Bootcamp-30.
+ * Autor                    : Oscar Candela.
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * Modificaciones
+ * Motivo                   Fecha             Nombre                  Descripción
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * Bootcamp-30              17/08/2022        Mario Vásquez           Realizar la creación de un método nuevo.
+ */
+
 package pe.com.nttdata;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Clase para iniciar el discovery.
+ */
 @SpringBootTest
 class SpringBootEurekaServerDiscoveryApplicationTest {
 
